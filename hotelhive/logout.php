cb860dfee961591630fa3019fabaa59d52b9f49a
Eternalic <lamperouge975@gmail.com>
2025-10-29 14:02:24 +0800

    Create logout.php

---
+++ hotelhive/logout.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+// Clear all session variables
+$_SESSION = array();
+
+// Destroy the session cookie
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time() - 3600, '/');
+}
+
+// Destroy the session
+session_destroy();
+
+// Return JSON response for AJAX request
+if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest') {
+    echo json_encode(['success' => true]);
+    exit;
+}
+
+// Redirect to homepage for normal request
+header("Location: homepage.php");
+exit;
+?> 
